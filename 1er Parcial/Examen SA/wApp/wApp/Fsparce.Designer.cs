0905fd91bbdcc16506e1338bb2f3ac84cfb6952b
Rene-Chungara <113743986+Rene-Chungara@users.noreply.github.com>
2023-01-19 18:08:53 -0400

    Add files via upload

---
+++ 1er Parcial/Examen SA/wApp/wApp/Fsparce.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace wApp
+{
+    partial class Fsparce
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Msparce = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMatriz = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPila = new System.Windows.Forms.Label();
+            this.lblPS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Msparce
+            // 
+            this.Msparce.Location = new System.Drawing.Point(64, 47);
+            this.Msparce.Name = "Msparce";
+            this.Msparce.Size = new System.Drawing.Size(86, 28);
+            this.Msparce.TabIndex = 0;
+            this.Msparce.Text = "MostrarSparce";
+            this.Msparce.UseVisualStyleBackColor = true;
+            this.Msparce.Click += new System.EventHandler(this.Msparce_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Z";
+            // 
+            // lblMatriz
+            // 
+            this.lblMatriz.AutoSize = true;
+            this.lblMatriz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatriz.Location = new System.Drawing.Point(79, 94);
+            this.lblMatriz.Name = "lblMatriz";
+            this.lblMatriz.Size = new System.Drawing.Size(17, 16);
+            this.lblMatriz.TabIndex = 2;
+            this.lblMatriz.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Mostrar PS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(456, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Pila con Diagonal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(232, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(452, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "S";
+            // 
+            // lblPila
+            // 
+            this.lblPila.AutoSize = true;
+            this.lblPila.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPila.Location = new System.Drawing.Point(479, 94);
+            this.lblPila.Name = "lblPila";
+            this.lblPila.Size = new System.Drawing.Size(17, 16);
+            this.lblPila.TabIndex = 8;
+            this.lblPila.Text = "...";
+            // 
+            // lblPS
+            // 
+            this.lblPS.AutoSize = true;
+            this.lblPS.Location = new System.Drawing.Point(261, 96);
+            this.lblPS.Name = "lblPS";
+            this.lblPS.Size = new System.Drawing.Size(16, 13);
+            this.lblPS.TabIndex = 9;
+            this.lblPS.Text = "...";
+            // 
+            // Fsparce
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 425);
+            this.Controls.Add(this.lblPS);
+            this.Controls.Add(this.lblPila);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMatriz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Msparce);
+            this.Name = "Fsparce";
+            this.Text = "Fsparce";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Msparce;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMatriz;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPila;
+        private System.Windows.Forms.Label lblPS;
+    }
+}
---
+++ 1er Parcial/Examen SA/wApp/wApp/Fsparce.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cApp;
+
+namespace wApp
+{
+    public partial class Fsparce : Form
+    {
+        public Fsparce()
+        {
+            InitializeComponent();
+        }
+        int[,] Z = {
+                     { 0, 0, 0 , 0, 0, 0 , 5},
+                     { 0, 0, 1 , 0, 0, 1 , 0},
+                     { 0, 0, 0 , 3, 0, 0 , 1},
+                     { 8, 0, 0 , 0, 0, 0 , 2},
+                     { 0, 0, 1 , 5, 0, 0 , 0},
+                     { 0, 5, 0 , 0, 0, 1 , 0},
+                     { 2, 0, 0 , 0, 0, 0 , 0},
+         };
+        clsSpar S = new clsSpar();
+        clsPila P = new clsPila();
+
+        private void Msparce_Click(object sender, EventArgs e)
+        {
+            S.n = 7;       // Fila de la matriz
+            S.m = 7;       // Columna la matriz
+            lblMatriz.Text = S.MostraArreglo(Z);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            S = S.CopyZtoPS(Z);
+            lblPS.Text = S.MostrarVector();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            P = S.DiagonalSegunda(S.PS);
+            lblPila.Text = P.Imprimir();
+        }
+
+        private void lblPS_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
